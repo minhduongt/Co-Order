35431b1a5a0e01d9cd5e5f6b671055af446fb05c
Endy <nguyenduong971999@gmail.com>
2022-10-16 02:35:09 +0700

    fix: filter api

---
+++ types/constant.ts
@@ -0,0 +1,27 @@
+export enum OrderStatusEnum {
+    WAITING = "WAITING",
+    FINISHED = "FINISHED",
+    CANCELED = " CANCELED",
+
+};
+export enum OrderTypeEnum {
+    NORMAL = "NORMAL",
+    PARTY = "PARTY",
+}
+export enum MenuDateFilterEnums {
+    MONDAY = "MONDAY",
+    TUESDAY = "TUESDAY",
+    WEDNESDAY = "WEDNESDAY",
+    THURSDAY = "THURSDAY",
+    FRIDAY = "FRIDAY",
+    SATURDAY = "SATURDAY",
+    SUNDAY = "SUNDAY",
+}
+
+export enum MenuHourFilterEnums {
+    BREAKFAST = "BREAKFAST",
+    BRUNCH = "BRUNCH",
+    LUNCH = "LUNCH",
+    TEA = "TEA",
+    DINNER = "DINNER",
+}
--- types/order.ts
+++ types/order.ts
@@ -3,39 +3,39 @@ import { TMenu } from "./menu";
 import { TTimeSlot } from "./timeslot";
 import { TCustomer } from "./user";
 
+// export type TOrder = {
+//   invoice_id: string;
+//   check_in_date: Date;
+//   check_out_date: Date;
+//   total_amount: number;
+//   discount: number;
+//   discount_order_detail: number;
+//   final_amount: number;
+//   order_status: number;
+//   customer_id: number;
+//   is_exported: boolean;
+//   time_slot: string;
+//   arrive_time: string;
+//   dest_location_id: number;
+//   batch_id: number;
+//   unibean_metadata: string;
+//   order_delivery_amount: number;
+//   // batch:	Batch{...},
+//   // customer:	Customer{...},
+//   // dest_location:	DestinationLocation{...}
+//   order_details: any[];
+//   payment: any[];
+//   store_notes: any[];
+//   id: number;
+//   active: boolean;
+//   created_by: string;
+//   updated_by: string;
+//   created_at: Date;
+//   updated_at: Date;
+// };
+
+
 export type TOrder = {
-  invoice_id: string;
-  check_in_date: Date;
-  check_out_date: Date;
-  total_amount: number;
-  discount: number;
-  discount_order_detail: number;
-  final_amount: number;
-  order_status: number;
-  customer_id: number;
-  is_exported: boolean;
-  time_slot: string;
-  arrive_time: string;
-  dest_location_id: number;
-  batch_id: number;
-  unibean_metadata: string;
-  order_delivery_amount: number;
-  // batch:	Batch{...},
-  // customer:	Customer{...},
-  // dest_location:	DestinationLocation{...}
-  order_details: any[];
-  payment: any[];
-  store_notes: any[];
-  id: number;
-  active: boolean;
-  created_by: string;
-  updated_by: string;
-  created_at: Date;
-  updated_at: Date;
-};
-
-
-export type TPartyOrder = {
   id: number;
   orderCode: string;
   type: string;
